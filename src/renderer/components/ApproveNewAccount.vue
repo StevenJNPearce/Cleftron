d107ab5143e1f32405929566dbf1f940624bf3bb
Steven Pearce <stevenpearce.uk@gmail.com>
2018-05-22 14:18:13 +0200

    Password field for ApproveNewAccount

--- src/renderer/components/ApproveNewAccount.vue
+++ src/renderer/components/ApproveNewAccount.vue
@@ -13,10 +13,21 @@ export default {
   components: {
     RequestInfo
   },
+  computed: {
+    passphrase: {
+      get () {
+        return store.state.passphrase
+      },
+      set (value) {
+        store.dispatch('addPassphrase', value);
+      }
+    }
+  },
   methods: {
     approve (evt) {
       const response = {
-        "approved" : true
+        "approved" : true,
+        "password" : this.passphrase
       }
       ipcRenderer.send('response',JSON.stringify(jsonrpc.success(store.state.pending.id, response)))
       store.dispatch('setUi', '');
@@ -36,7 +47,12 @@ export default {
     <b-form>
         <b-card title="Approve New Account" bg-variant="light">
             <RequestInfo></RequestInfo>
-            
+              <b-form-group horizontal
+                            label="Password:"
+                            label-class="text-sm-right"
+                            label-for="pass">
+                <b-form-input type="password" v-model="passphrase" id="pass"></b-form-input>
+              </b-form-group>
               <b-container>
                 <b-row class="text-center">
                     <b-col class="py-3">
